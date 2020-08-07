c2237f27dcd152a481af1ea97d6810b4b2852322
Marek Defeciński <marekdef@gmail.com>
2020-08-07 23:09:01 +0200

    Someone is wrong on the Internet

--- app/src/androidTest/java/com/example/todoapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/todoapp/ExampleInstrumentedTest.kt
@@ -8,6 +8,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Ignore
 import kotlin.concurrent.thread
 
 /**
@@ -18,7 +19,7 @@ import kotlin.concurrent.thread
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
+    fun testIfCallsOnTwoThreadsReturnSameInstance() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.todoapp", appContext.packageName)
@@ -26,6 +27,12 @@ class ExampleInstrumentedTest {
         var instance1: ToDoDatabase? = null
         var instance2: ToDoDatabase? = null
         val thread1 = thread {
+            /*
+            Add sleep to make this test pass.
+            This adds necessary time so threads do not execute
+            `if (tempInstance != null)` at the same time
+            Thread.sleep(5_000)
+             */
             instance1 = ToDoDatabase.getDatabase(appContext)
         }
 
@@ -37,6 +44,22 @@ class ExampleInstrumentedTest {
         thread1.join()
 
         assertSame(instance1, instance2)
+    }
 
+
+    /*
+    If you remove @Ignore this test will pass because here singleton is initialized properly
+     */
+    @Ignore
+    @Test
+    fun testIfCallsInSequenceReturnSameInstance() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.todoapp", appContext.packageName)
+
+        val instance1: ToDoDatabase = ToDoDatabase.getDatabase(appContext)
+        val instance2: ToDoDatabase  = ToDoDatabase.getDatabase(appContext)
+
+        assertSame(instance1, instance2)
     }
 }
