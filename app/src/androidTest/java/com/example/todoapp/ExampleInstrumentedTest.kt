88187708d98f92b22e10cf4c7c6adb61f80f833a
Marek Defeciński <marekdef@gmail.com>
2020-08-07 22:59:22 +0200

    Someone is wrong on the Internet

--- app/src/androidTest/java/com/example/todoapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/todoapp/ExampleInstrumentedTest.kt
@@ -2,11 +2,13 @@ package com.example.todoapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.todoapp.data.ToDoDatabase
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import kotlin.concurrent.thread
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +22,21 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.todoapp", appContext.packageName)
+
+        var instance1: ToDoDatabase? = null
+        var instance2: ToDoDatabase? = null
+        val thread1 = thread {
+            instance1 = ToDoDatabase.getDatabase(appContext)
+        }
+
+        val thread2 = thread {
+            instance2 = ToDoDatabase.getDatabase(appContext)
+        }
+
+        thread2.join()
+        thread1.join()
+
+        assertSame(instance1, instance2)
+
     }
 }
